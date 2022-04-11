e8fc9df6f983f22d767a6368946ae5357a25bfc4
Lucas Ferreira <bioinfolucas@gmail.com>
2022-04-11 16:01:17 -0400

    being5

--- homer_motif.py
+++ homer_motif.py
@@ -1,41 +1,41 @@
 
 def install_homer_colab_hg19():
-'''
-Description:
-    This function  will install homer  and the hg19 genome on your colab.
-    The genome is downloaded from UCSC and the hg19 genome is installed on your colab.
-    The homer will be installed in the bin folder.
-    And will be invoked by using the command:
-    perl bin/findMotifsGenome.pl and the genome will be installed in the data/genomes/hg19
-'''
+  '''
+  Description:
+      This function  will install homer  and the hg19 genome on your colab.
+      The genome is downloaded from UCSC and the hg19 genome is installed on your colab.
+      The homer will be installed in the bin folder.
+      And will be invoked by using the command:
+      perl bin/findMotifsGenome.pl and the genome will be installed in the data/genomes/hg19
+  '''
   !wget http://homer.ucsd.edu/homer/configureHomer.pl
+  !perl configureHomer.pl -install 
   !perl configureHomer.pl -install hg19
   x_p = !echo $PATH
-  final_p  = x_p[0] + ':/content/.//bin/' 
+  final_p  = x_p[0] + ':content/./bin/' 
   %env PATH= $final_p
 
 
-
 def create_one_by_one_motif(bed_file, out_name ):
-'''
-Description:
-    This function will create one by one motifs from the bed file and return in a pandas DataFrame.
-    The bed file should be in the format:
-    chr start end motif_name
-    The output will be a pandas DataFrame with the following columns:
-Parameters:
-    bed_file: the bed file with the format: chr start end motif_name
-    out_name: the name of the output file
-return:
-    pandas DataFrame with the following columns:
-    motif_name: the name of the motif
-    motif_length: the length of the motif
-    
-'''
-  df_process_one = pd.read_csv(bed_file, sep='\t')
-  
+  '''
+  Description:
+      This function will create one by one motifs from the bed file and return in a pandas DataFrame.
+      The bed file should be in the format:
+      chr start end motif_name
+      The output will be a pandas DataFrame with the following columns:
+  Parameters:
+      bed_file: the bed file with the format: chr start end motif_name (This files can't have a header)
+      out_name: the name of the output file
+  return:
+      pandas DataFrame with the following columns:
+      motif_name: the name of the motif
+      motif_length: the length of the motif
+      
+  '''
+  df_process_one = pd.read_csv(bed_file, sep='\t', header=None)
+  df_process_one.columns = [str(c) for c in df_process_one.columns ]
   df_process_one['3'] = df_process_one[['0','1','2']].apply(lambda x : '_'.join(map(str,x)) ,axis=1)
-  bed_file_use = 'tag_added_' + bed_file
+  bed_file_use = bed_file
   df_process_one.to_csv(bed_file_use,
                          index=None,
                          header=None, sep='\t')
